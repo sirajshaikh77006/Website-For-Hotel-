d043bbb5c8a025f64788fc97c5df9cd249e377b9
Siraj <shaikhsiraj@eng.rizvi.edu.in>
2025-10-06 11:49:18 +0530

    create navbar and set routes for home cart and place order pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/NavBar/Navbar";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
